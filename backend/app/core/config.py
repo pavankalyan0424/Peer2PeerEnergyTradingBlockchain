38587e55def6ac5892d5b416949e1d6daa42bf74
pavankalyan0424 <addepalli.005@gmail.com>
2026-08-01 10:59:35 +0530

    Fix blockchain issues

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,6 +9,6 @@ BUYER_PRIVATE_KEY = os.getenv("BUYER_PRIVATE_KEY") or ""
 BACKEND_ROOT = Path(__file__).resolve().parents[2]
 CONFIG_DIR = BACKEND_ROOT / "config"
 BLOCKCHAIN_PROJECT_ROOT = (
-    BACKEND_ROOT.parent / "blockchain_docker"
-)  # TODO: rename this later
+    BACKEND_ROOT.parent / "blockchain"
+)  
 FOUNDRY_OUT_DIR = BLOCKCHAIN_PROJECT_ROOT / "out"
